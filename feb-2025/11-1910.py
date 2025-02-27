fb4450dd3894466f101329333a3af2a8c7ef935f
naomitzhao <116607148+naomitzhao@users.noreply.github.com>
2025-02-26 22:43:25 -0800

    feat: Reorganize files

--- feb-2025/11-1910.py
+++ feb-2025/11-1910.py
@@ -23,4 +23,19 @@ class Solution:
             
         
         return ''.join(new_string)
+
+
+# much better solution using stack only and string slicing
+class Solution:
+    def removeOccurrences(self, s: str, part: str) -> str:
+        new_string = []
+
+        for ch in s:
+            new_string.append(ch)
+            if len(new_string) >= len(part):
+                start_try = len(new_string) - len(part)
+                if ''.join(new_string[start_try : len(new_string)]) == part:
+                    new_string = new_string[: start_try]
+            
+        return ''.join(new_string)
             
