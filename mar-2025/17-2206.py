a885b437900433939e06db5cad369c53ef86ddee
naomitzhao <116607148+naomitzhao@users.noreply.github.com>
2025-03-17 15:06:31 -0700

    feat: Daily solution

---
+++ mar-2025/17-2206.py
@@ -0,0 +1,10 @@
+class Solution:
+    def divideArray(self, nums: List[int]) -> bool:
+        unpaired = set()
+        for num in nums:
+            if num in unpaired:
+                unpaired.remove(num)
+            else:
+                unpaired.add(num)
+        
+        return len(unpaired) == 0
