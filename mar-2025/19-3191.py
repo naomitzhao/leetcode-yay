44b6227592f1161b08dd8fa454510a85d195eeff
naomitzhao <116607148+naomitzhao@users.noreply.github.com>
2025-03-19 17:31:08 -0700

    feat: Daily solution, 3191

---
+++ mar-2025/19-3191.py
@@ -0,0 +1,13 @@
+class Solution:
+    def minOperations(self, nums: List[int]) -> int:
+        count = 0
+        for i in range(len(nums) - 2):
+            if nums[i] == 0:
+                count += 1
+                nums[i] = nums[i] ^ 1
+                nums[i + 1] = nums[i + 1] ^ 1
+                nums[i + 2] = nums[i + 2] ^ 1
+        
+        if sum(nums) == len(nums):
+            return count
+        return -1
